769c836474cb98dd08bb2512b01d4b66187d9169
Leonardo Guerra <leonardo.bezerra.guerra@gmail.com>
2022-09-03 19:34:07 -0300

    implementando a funcionalidade de paralizar o jogo caso o mario encoste no pipe

---
+++ script.js
@@ -0,0 +1,29 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+
+const jump = () => {
+   
+    mario.classList.add('jump');
+
+    setTimeout(()=>{
+        mario.classList.remove('jump')
+    }, 500)
+    
+}
+
+const loop = setInterval(()=> {
+    const pipePosition = pipe.offsetLeft;
+    //captura o bottom atual do mário e converte para int o "+" faz a convertion
+    const marioHightJump = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+   
+   if (pipePosition <= 120 && pipePosition > 0  && marioHightJump < 80){
+    pipe.style.animation = 'none';
+    pipe.style.left= `${pipePosition}px`;
+   }
+}, 10)
+
+document.addEventListener('keydown', jump);
+
+console.log("carregou o script")
